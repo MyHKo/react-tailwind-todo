05170ceee676b7481742a514f665f83dd1ef29f2
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-03-12 18:54:43 +0100

    Fix Styles in Title component

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -1,6 +1,6 @@
 function Title() {
     return (
-        <div className="flex items-center justify-center w-parent h-13 bg-gray-700">
+        <div className="flex items-center justify-center rounded-lg w-parent h-13 bg-gray-700">
             <h1 className={"text-gray-200 text-3xl font-bold"}>Make Your Own To-Do List</h1>
         </div>
     )
