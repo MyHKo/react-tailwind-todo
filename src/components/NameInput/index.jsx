02b4bca47d8e40b63358394379c910d85090dec3
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-03-13 11:23:30 +0100

    Feat: add mobile version of app

--- src/components/NameInput/index.jsx
+++ src/components/NameInput/index.jsx
@@ -6,12 +6,12 @@ function NameInput() {
     const input = useRef(null)
 
     return (
-        <div className="flex flex-row justify-between gap-5 mt-5 mb-5 h-13">
+        <div className="flex flex-row justify-between gap-2 sm:gap-5 mt-5 mb-5 h-13">
             <div className="flex items-center rounded-lg p-2 w-full bg-gray-700">
                 <input className="text-gray-200 text-2xl h-full w-full focus:outline-none" ref={input}/>
             </div>
             <button
-                className="text-gray-200 text-xl font-bold rounded-lg h-full w-40 bg-green-500 hover:bg-green-400 active:bg-green-500 cursor-pointer"
+                className="text-gray-200 text-xl font-bold rounded-lg h-full w-20 sm:w-40 bg-green-500 hover:bg-green-400 active:bg-green-500 cursor-pointer"
                 onClick={() => {
                     if(input.current.value !== "") {
                         addTask(input.current.value)
--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -7,8 +7,8 @@ function Task({id, name, done}) {
     const [, , deleteTask] = useContext(ListContext)
 
     const nameStyle = isDone
-    ? `flex text-gray-200 text-2xl line-through w-full items-center rounded-lg p-2 bg-gray-500`
-    : `flex text-gray-200 text-2xl w-full items-center rounded-lg p-2 bg-gray-700`
+    ? `flex text-gray-200 text-2xl line-through h-13 w-full items-center rounded-lg p-2 bg-gray-400`
+    : `flex text-gray-200 text-2xl h-13 w-full items-center rounded-lg p-2 bg-gray-700`
 
     const handleDelete = () => {
         deleteTask(id)
@@ -19,22 +19,24 @@ function Task({id, name, done}) {
     }
 
     return (
-        <div id={id} className="flex flex-row justify-between gap-5 mt-5 mb-5 h-13">
+        <div id={id} className="flex flex-col sm:flex-row gap-5 mt-5 mb-15 sm:mb-5 h-20 sm:h-13">
             <div className={nameStyle}>
                 {name}
             </div>
-            <button
-                className="text-gray-200 text-xl font-bold rounded-lg h-full w-30 bg-blue-500 hover:bg-blue-400 active:bg-blue-500 cursor-pointer"
-                onClick={() => {toggleDone()}}
-            >
-                {isDone? "Undo" : "Done"}
-            </button>
-            <button
-                className="text-gray-200 text-xl font-bold rounded-lg h-full w-40 bg-red-500 hover:bg-red-400 active:bg-red-500 cursor-pointer"
-                onClick={() => {handleDelete()}}
-            >
-                Remove
-            </button>
+            <div className="flex gap-5">
+                <button
+                    className="text-gray-200 text-xl font-bold rounded-lg h-full w-20 sm:w-30 bg-blue-500 hover:bg-blue-400 active:bg-blue-500 cursor-pointer"
+                    onClick={() => {toggleDone()}}
+                >
+                    {isDone? "Undo" : "Done"}
+                </button>
+                <button
+                    className="text-gray-200 text-xl font-bold rounded-lg h-10 sm:h-full w-20 sm:w-40 bg-red-500 hover:bg-red-400 active:bg-red-500 cursor-pointer"
+                    onClick={() => {handleDelete()}}
+                >
+                    Remove
+                </button>
+            </div>
         </div>
     )
 }
