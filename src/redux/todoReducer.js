9da99cb5daacadea53678a2b9833fcbb3cc06cb1
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-04-08 16:31:00 +0200

    Fix bugs in reducers

--- src/redux/todoReducer.js
+++ src/redux/todoReducer.js
@@ -6,14 +6,19 @@ const initialState = {
 
 function todoReducer(state = initialState, action) {
     switch (action.type) {
-        case 'ADD_TOD':
-            return {todos: [state, {
+        case 'ADD_TODO':
+            return {
+                todos: [...state.todos,{
                     id:nanoid(),
                     name: action.payload,
                     done: false
-            }]}
+                        }
+                    ]
+            }
         case 'REMOVE_TODO':
-            return state.todos.filter(task => task.id !== action.payload)
+            return {
+                todos: state.todos.filter(task => task.id !== action.payload)
+            }
         default:
             return state;
     }
