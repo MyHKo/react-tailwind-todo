10c381a7121de3498b7509fde452de8ac04dc423
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-04-09 18:10:54 +0200

    Create a reducer for toggling todo

--- src/redux/reducers/todoReducer.js
+++ src/redux/reducers/todoReducer.js
@@ -1,5 +1,5 @@
 import {nanoid} from "nanoid";
-import {ADD_TODO, REMOVE_TODO} from "../constraints.js";
+import {ADD_TODO, REMOVE_TODO, TOGGLE_TODO} from "../constraints.js";
 
 const initialState = {
     todos: []
@@ -19,6 +19,16 @@ function todoReducer(state = initialState, action) {
         case REMOVE_TODO:
             return {
                 todos: state.todos.filter(task => task.id !== action.payload)
+            }
+        case TOGGLE_TODO:
+            return {
+                todos: state.todos.map((todo) => {
+                    if (todo.id === action.payload) {
+                        return { ...todo, done: !todo.done };
+                    } else {
+                        return todo;
+                    }
+                })
             }
         default:
             return state;
